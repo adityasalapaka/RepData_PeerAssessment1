5cc88f46402b013be980cdfd8113e5334e49b273
adityasalapaka <salapaka.aditya@gmail.com>
2014-12-23 18:06:11 +0530

    Ignore *.db, added script

---
+++ fuelband.R
@@ -0,0 +1,32 @@
+data <- read.csv("activity.csv")
+
+x <- aggregate(data$steps, by = list(Date = data$date), FUN = sum)
+
+hist(x$x)
+
+mean(x$x, na.rm = TRUE)
+median(x$x, na.rm = TRUE)
+
+data2 <- data[with(data, order(interval)),]
+data2 <- data2[complete.cases(data2),]
+
+y <- aggregate(data2$steps[complete.cases(data2$steps)], by = list(interval = data2$interval), FUN = mean)
+
+plot(y, type = "l")
+y$interval[which(y$x == max(y$x))]
+
+library("plyr")
+data3 <- data[with(data, order(interval)),]
+impute.mean <- function(x) replace(x, is.na(x), mean(x, na.rm = TRUE))
+data3 <- ddply(data3, ~ interval, transform, steps = impute.mean(steps))
+data3 <- data3[order(data3$interval),]
+
+z <- aggregate(data3$steps, by = list(Date = data3$date), FUN = sum)
+hist(z$x)
+
+mean(z$x, na.rm = TRUE)
+median(z$x, na.rm = TRUE)
+
+weekend <- c("Saturday", "Sunday")
+
+data3$daytype = as.factor(ifelse(is.element(weekdays(as.Date(data3$date)),weekend), "Weekend", "Weekday"))
